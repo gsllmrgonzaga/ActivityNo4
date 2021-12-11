5de52dff74110c49f6876a6eca2a40b8ca323eb8
unknown <gsllmrgonzaga>
2021-12-12 01:20:40 +0800

    added code for get product by id

--- app/src/main/java/com/example/gonzaga/data/DatabaseHandler.java
+++ app/src/main/java/com/example/gonzaga/data/DatabaseHandler.java
@@ -1,12 +1,15 @@
 package com.example.gonzaga.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 import com.example.gonzaga.R;
+import com.example.gonzaga.model.Product;
 import com.example.gonzaga.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -33,8 +36,40 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
 
     //CREATE
+    public void addProduct(Product product)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, product.getName());
+        values.put(Util.KEY_PRICE, product.getPrice());
+        values.put(Util.KEY_QUANTITY, product.getQuantity());
+
+        db.insert(Util.TABLE_NAME, null, values);
+        db.close();
+    }
 
     //RETRIEVE
+    public Product getProduct(int id)
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(Util.TABLE_NAME,new String[] {Util.KEY_ID, Util.KEY_NAME, Util.KEY_PRICE, Util.KEY_QUANTITY},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)},
+                null, null, null, null);
+
+        if(cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Product product = new Product();
+        product.setId(cursor.getInt(0));
+        product.setName(cursor.getString(1));
+        product.setPrice(cursor.getLong(2));
+        product.setQuantity(cursor.getInt(3));
+
+        return product;
+    }
+
 
     //UPDATE
 
