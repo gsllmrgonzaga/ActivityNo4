68b0dafdcc2cdb6cc7ca10321b0292cfc1aae0e5
unknown <gsllmrgonzaga>
2021-12-12 00:34:42 +0800

    added codes for dbhandler oncreate and onupgrade

---
+++ app/src/main/java/com/example/gonzaga/util/Util.java
@@ -0,0 +1,12 @@
+package com.example.gonzaga.util;
+
+public class Util {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "product_db";
+    public static final String TABLE_NAME = "products";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PRICE = "price";
+    public static final String KEY_QUANTITY = "quantity";
+}
